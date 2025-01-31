ffc5f62f0e58eebe3c21817571415a2c0c7a0364
Syed Umar Shah <syedumarshah8888@gmail.com>
2025-01-31 10:32:02 -0800

    use bcryptjs instead of bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js"
 import validator from 'validator'
-import bcrypt from 'bcrypt'
+import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 
@@ -21,7 +21,7 @@ const loginUser = async (req, res) => {
             return res.json({success: false, message:"User doesn't exist"})
         }
 
-        const isMatch = await bcrypt.compare(password, user.password)
+        const isMatch = await bcryptjs.compare(password, user.password)
 
         if(isMatch) {
             const token = createToken(user._id)
@@ -59,8 +59,8 @@ const registerUser = async (req, res) => {
         }
 
         // Hashing user password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
 
         const newUser = new userModel({
             name, 
