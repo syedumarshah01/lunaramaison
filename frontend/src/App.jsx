c9200efbe80fd6a17a074c5f5bf2a5d04e734eb4
syedumarshah01 <syedumarshahgithub@gmail.com>
2025-02-04 14:17:20 -0800

    Update App.jsx
    remove "observer.unobserve(entry.target)
        }" to keep observing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,8 +22,7 @@ const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if(entry.isIntersecting) {
       entry.target.classList.add('visible')
-      observer.unobserve(entry.target)
-    }
+      
   })
 }, {threshold: 0.2})
 
