2d0a3f9b2e76bc93beba219ba1a68ca8070cfe55
syedumarshah01 <syedumarshahgithub@gmail.com>
2025-03-08 11:06:07 -0800

    Update Contact.jsx
    add lazy attribute to img tag in contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
       </div>
 
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
-        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} loading="lazy" alt="contact" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
           <p className='text-gray-500'>Khazana Sugar Mills, <br /> Charsadda Road, Peshawar</p>
