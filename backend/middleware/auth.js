8496e0d9479df77d242f1ce9ee824c4bf37cebda
syedumarshah01 <syedumarshahgithub@gmail.com>
2025-06-07 00:17:40 -0700

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -12,7 +12,7 @@ const authUser = async (req, res, next) => {
         next()
     } catch (error) {
         console.log(error)
-        req.json({success: false, message: error.message})
+        res.json({success: false, message: error.message})
     }
 }
 
